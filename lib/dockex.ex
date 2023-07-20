38397ef5874d85a3ccb6e8aecdd7e950a385395e
cognivore <66186054+cognivore@users.noreply.github.com>
2023-07-20 12:38:55 +0100

    Initial commit

---
+++ lib/dockex.ex
@@ -0,0 +1,5 @@
+defmodule Dockex do
+  @moduledoc """
+  Integration module for Dockex. 
+  """
+end
---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule Dockex.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :dockex,
+      version: "1.0.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1.0", [runtime: false]},
+      {:uptight, github: "doma-engineering/uptight", branch: "main"},
+      {:quark_goo, github: "doma-engineering/quark-goo", branch: "main"},
+      {:algae_goo, github: "doma-engineering/algae-goo", branch: "main"},
+      {:witchcraft_goo, github: "doma-engineering/witchcraft-goo", branch: "main"}
+    ]
+  end
+end
